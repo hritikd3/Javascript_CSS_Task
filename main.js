0ee3bf40debca7f2b7621e9417f41311f9463256
hritikd3 <hritikdangi3@gmail.com>
2023-09-17 16:35:19 +0530

    updates in logic

--- main.js
+++ main.js
@@ -1,10 +1,63 @@
-var rect= document.querySelector("#center")
+// const rect = document.querySelector("#center");
 
-rect.addEventListener('mousemove', (details)=>{
-    // console.log("cursor is moving ")
-const rectLocation= rect.getBoundingClientRect();
+// rect.addEventListener("mousemove", function (details) {
+//   const rectLocation = rect.getBoundingClientRect();
 
-console.log(details.clientX - rectLocation.left)
+//   const insideRectangeValue = Math.trunc(details.clientX - rectLocation.left);
 
-    // alert("yes the mouse is hovering ")
-})
+//   if (insideRectangeValue < rectLocation.width / 2) {
+//     var redcolor = gsap.utils.mapRange(
+//       0,
+//       rectLocation.width / 2,
+//       255,
+//       0,
+//       insideRectangeValue
+//     );
+//     gsap.to(rect, {
+//       backgroundColor: `rgb(${redcolor},0,0),
+//     ease: Power4`,
+//     });
+//     // console.log("ew");
+//   } else {
+//     var bluecolor = gsap.utils.mapRange(
+//       rectLocation.width / 2,
+//       rectLocation.width,
+//       0,
+//       255,
+//       insideRectangeValue
+//     );
+//     gsap.to(rect, {
+//       backgroundColor: `rgb(${bluecolor},0,0),
+//     ease: Power4`,
+//     });
+// // console.log("Ds")
+//   }
+
+// });
+
+
+
+// rect.addEventListener("mouseleave",()=>{
+//   gsap.to(rect,{
+//     backgroundColor: 'white'
+//   })
+// })
+
+const div = document.querySelector("#center");
+
+div.addEventListener("mousemove", (event) => {
+  const centerX = div.offsetLeft + div.offsetWidth / 2;
+
+  if (event.clientX < centerX) {
+    // Moving left of the center
+    div.style.backgroundColor = `rgb(255, ${Math.trunc(
+      (event.clientX / centerX) * 255
+    )}, 0)`;
+  } else {
+    // Moving right of the center
+    const blueShade = Math.trunc(
+      255 - ((event.clientX - centerX) / (div.offsetWidth / 2)) * 255
+    );
+    div.style.backgroundColor = `rgb(0, 0, ${blueShade})`;
+  }
+});
