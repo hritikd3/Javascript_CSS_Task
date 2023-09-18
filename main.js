0e180121593006f3364d695e2c8b665805b42aab
hritikd3 <hritikdangi3@gmail.com>
2023-09-18 11:31:48 +0530

    dom event for the clientX value

--- main.js
+++ main.js
@@ -1,63 +1,36 @@
-// const rect = document.querySelector("#center");
-
-// rect.addEventListener("mousemove", function (details) {
-//   const rectLocation = rect.getBoundingClientRect();
-
-//   const insideRectangeValue = Math.trunc(details.clientX - rectLocation.left);
-
-//   if (insideRectangeValue < rectLocation.width / 2) {
-//     var redcolor = gsap.utils.mapRange(
-//       0,
-//       rectLocation.width / 2,
-//       255,
-//       0,
-//       insideRectangeValue
-//     );
-//     gsap.to(rect, {
-//       backgroundColor: `rgb(${redcolor},0,0),
-//     ease: Power4`,
-//     });
-//     // console.log("ew");
-//   } else {
-//     var bluecolor = gsap.utils.mapRange(
-//       rectLocation.width / 2,
-//       rectLocation.width,
-//       0,
-//       255,
-//       insideRectangeValue
-//     );
-//     gsap.to(rect, {
-//       backgroundColor: `rgb(${bluecolor},0,0),
-//     ease: Power4`,
-//     });
-// // console.log("Ds")
-//   }
-
-// });
-
-
-
-// rect.addEventListener("mouseleave",()=>{
-//   gsap.to(rect,{
-//     backgroundColor: 'white'
-//   })
-// })
-
 const div = document.querySelector("#center");
+const clientXValue = document.querySelector("#clientXValue");
 
 div.addEventListener("mousemove", (event) => {
   const centerX = div.offsetLeft + div.offsetWidth / 2;
 
   if (event.clientX < centerX) {
-    // Moving left of the center
-    div.style.backgroundColor = `rgb(255, ${Math.trunc(
-      (event.clientX / centerX) * 255
-    )}, 0)`;
-  } else {
-    // Moving right of the center
+    // Moving to the left
+    const redShade = Math.trunc(
+      ((centerX - event.clientX) / (div.offsetWidth / 2)) * 255
+    );
+    div.style.backgroundColor = `rgb(255, ${redShade}, 0)`;
+  } else if (event.clientX > centerX) {
+    // Moving to the right
     const blueShade = Math.trunc(
-      255 - ((event.clientX - centerX) / (div.offsetWidth / 2)) * 255
+      ((event.clientX - centerX) / (div.offsetWidth / 2)) * 255
     );
     div.style.backgroundColor = `rgb(0, 0, ${blueShade})`;
+  } else {
+    // Mouse is exactly at the center
+    div.style.backgroundColor = "black";
   }
+
+  // Display clientX value on the screen
+  clientXValue.textContent = `clientX: ${event.clientX}`;
+  clientXValue.style.color= " purple";
+  clientXValue.style.fontSize= "40px";
+
+});
+
+div.addEventListener("mouseleave", () => {
+  // Mouse leaves the div
+  div.style.backgroundColor = "white";
+  div.style.border = "2px solid black";
+  clientXValue.textContent = ""; // Clear the clientX value
 });
